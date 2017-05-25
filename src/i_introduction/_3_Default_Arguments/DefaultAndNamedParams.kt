89c0aa7718e4099ec433536e5571fac25f45cad1
David Moulton <dave@themoultons.net>
2017-05-25 06:49:32 -0600

    intro 9
    up through introduction 9

--- src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
+++ src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
@@ -14,12 +14,26 @@ fun todoTask3(): Nothing = TODO(
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String): String = todoTask3()
+fun foo(name: String,
+        number: Int = -1,
+        toUpperCase: Boolean = false): String {
+    val s = if (number < 0) {
+        name
+    } else {
+        name + number.toString()
+    }
+
+    return if (toUpperCase) {
+        s.toUpperCase()
+    } else {
+        s
+    }
+}
 
 fun task3(): String {
-    todoTask3()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    //todoTask3()
+    return (foo("a", number = 42) +
+            foo("b", number = 1) +
+            foo("c", number = 42, toUpperCase = true) +
+            foo(name = "d", number = 2, toUpperCase = true))
 }
